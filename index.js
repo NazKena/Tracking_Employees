c4e640d4f83f719767008146e80b5a3a51bb6552
Nuseyba <n.kenadiid@gmail.com>
2023-03-06 14:00:41 +0000

    added questions

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+const { prompt } = require("inquirer");
 const db = require("./db/connection.js");
+
+function openingQuestion() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Departments",
+          value: "viewAllDepartments",
+        },
+        {
+          name: "View All Employees",
+          value: "viewAllEmployees",
+        },
+        {
+          name: "View All Roles",
+          value: "viewAllRoles",
+        },
+        {
+          name: "Add A Department",
+          value: "addDepartment",
+        },
+        {
+          name: "Add A Role",
+          value: "addRole",
+        },
+        {
+          name: "Add An Employee",
+          value: "addEmployee",
+        },
+        { name: "Quit", value: "Quit" },
+      ],
+    },
+  ]);
+}
 
