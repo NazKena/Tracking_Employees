6820a5731d812c35b8aecf340e7db25bfdf29e90
Nuseyba <n.kenadiid@gmail.com>
2023-03-06 13:28:46 +0000

    complete seeds.sql

---
+++ db/seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (department_name)
+VALUES 
+("Sales"),
+("Legal"),
+("Finance"),
+("Marketing"),
+
+INSERT INTO role (title, id, salary, department_id)
+VALUES
+("Customer Service Agent",3,40000,),
+("Finance Manager",4,80000),
+("Sales Representative",5,35000),
+("Accountant",6,65000),
+("Sales Manager",2,60000),
+("Lawyer",,4,50000),
+
+INSERT INTO (first_name,last_name,,id,manager_id)
+VALUES
+("Shallan", "Davar",1,2),
+("Adolin", "Kholin",2,2),
+("Julian", "Blackthorn",3,NULL),
+("Robb","Stark",3, NULL),
+("Jem","Carstairs",3,2),
+("Kaladin", "Stormblessed",4,2),
+("Petyr", "Baelish",2,NULL),
+("Nesta", "Archeron",6,3,);
