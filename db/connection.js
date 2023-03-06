1513b2f07c3dda6a2d486a6cc07dcf09756200c7
Nuseyba <n.kenadiid@gmail.com>
2023-03-06 15:54:04 +0000

    added type:module to package.json

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password:"ABC",
+    password:"password",
     database: "employees_db",
 },
 
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const db = require("./db/connection.js");
 
 const teamArray = [];
 
-
 function openingQuestion() {
   prompt([
     {
@@ -44,8 +43,7 @@ function openingQuestion() {
   ]);
 
   function addDepartment() {
-    inquirer
-    .prompt([
+    inquirer.prompt([
       {
         type: "input",
         name: "departmentName",
@@ -53,139 +51,127 @@ function openingQuestion() {
       },
     ]);
 
+    then((answers) => {
+      const department = new Department(answers.departmentName);
+      teamArray.push(department);
+      init();
+    });
+  }
 
-    .then ((answers) => {
-        const department = new Department(
-            answers.departmentName,
-        );
-        teamArray.push(department)
-        init ()
-    }
-    )
-}
+  function addRole() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "Enter the name of role here",
+      },
 
-    function addRole() {
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "roleName",
-          message: "Enter the name of role here",
-        },
+      {
+        type: "input",
+        name: "salary",
+        message: "Enter salary for role here",
+      },
 
-        {
-          type: "input",
-          name: "salary",
-          message: "Enter salary for role here",
-        },
+      {
+        type: "input",
+        name: "department",
+        message: "Which department does the role belong top?",
+      },
+    ]);
 
-        {
-          type: "input",
-          name: "department",
-          message: "Which department does the role belong top?",
-        },
-      ]);
-
-    
-
-      .then ((answers) => {
-        const role = new Role(
-            answers.roleName,
-            answers.salary,
-            answers.department,
-        );
-        teamArray.push(role)
-        init ()
-    }
-    )
-}
+    then((answers) => {
+      const role = new Role(
+        answers.roleName,
+        answers.salary,
+        answers.department
+      );
+      teamArray.push(role);
+      init();
+    });
+  }
 
   function addEmployee() {
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "firstName",
-          message: "Enter the first name of employee here",
-        },
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Enter the first name of employee here",
+      },
 
-        {
-          type: "input",
-          name: "lastName",
-          message: "Enter the last name of employee here",
-        },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Enter the last name of employee here",
+      },
 
-        {
-          type: "input",
-          name: "role",
-          message: "What is the employee's role?",
-        },
-        {
-            type: "input",
-            name: "manager",
-            message: "What is the employee's manager?",
-          },
+      {
+        type: "input",
+        name: "role",
+        message: "What is the employee's role?",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "What is the employee's manager?",
+      },
+    ]);
 
-      ]);
-
-
-
-      .then ((answers) => {
-        const employee = new Employee(
-            answers.firstName,
-            answers.lastName,
-            answers.role,
-            answers.manager
-        );
-        teamArray.push(employee)
-        init ()
-    }
-    )
-}
-function init () {
-    inquirer
-    .prompt ([
-        {
-         type:'list',
-         name:"mainSelection",
-         message: "What would you like to do?",
-         choices:["View All Departments",
-         "View All Employees",
-         "View All Roles",
-         "Add A Department",
-         "Add A Role",
-         "Add An Employee",
-         "Quit",
-        ]
-        }   
-    ])
-
-
-      then((answer) => {
-        switch (answer.roles) {
-          case "viewAllDepartments":
-            viewAllDepartments();
-            break;
-          case "viewAllRoles":
-            viewAllRoles();
-            break;
-          case "viewAllEmployees":
-            viewAllEmployees();
-            break;
-          case "viewAllEmployees":
-            viewAllEmployees();
-            break;
-          case "addDepartment":
-            addDepartment();
-            break;
-          case "addRole":
-            addRole();
-            break;
-          case "addEmployee":
-            addEmployee();
-            break;
-          case "Finish":
-            addFinish();
-            break;
-        }
-      });
-    }
+    then((answers) => {
+      const employee = new Employee(
+        answers.firstName,
+        answers.lastName,
+        answers.role,
+        answers.manager
+      );
+      teamArray.push(employee);
+      init();
+    });
   }
+  function init() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "mainSelection",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Employees",
+          "View All Roles",
+          "Add A Department",
+          "Add A Role",
+          "Add An Employee",
+          "Quit",
+        ],
+      },
+    ]);
+
+    then((answer) => {
+      switch (answer.roles) {
+        case "viewAllDepartments":
+          viewAllDepartments();
+          break;
+        case "viewAllRoles":
+          viewAllRoles();
+          break;
+        case "viewAllEmployees":
+          viewAllEmployees();
+          break;
+        case "viewAllEmployees":
+          viewAllEmployees();
+          break;
+        case "addDepartment":
+          addDepartment();
+          break;
+        case "addRole":
+          addRole();
+          break;
+        case "addEmployee":
+          addEmployee();
+          break;
+        case "Finish":
+          addFinish();
+          break;
+      }
+    });
+  }
+}
